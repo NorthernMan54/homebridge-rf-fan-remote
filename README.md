41c905251770b5c7c998a3ad21983709e834b952
NorthernMan54 <buyminivan-northern@yahoo.ca>
2017-12-02 14:02:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,130 @@
+# homebridge-rf-fan-remote Plugin
+
+[![NPM Downloads](https://img.shields.io/npm/dm/homebridge-http-irblaster.svg?style=flat)](https://npmjs.org/package/homebridge-http-irblaster)
+
+I wrote this plugin as a wrapper around mdhiggins ESP8266-HTTP-IR-Blaster to allow remote control of InfraRed controlled devices by HomeKit. To use this plugin, you need to build the ESP8266 based IR Blaster device documented by mdhiggins on the github for the blaster. Once you have it built, you can control it via HomeKit.
+
+# Installation
+
+More detailed instructions are available here https://www.instructables.com/id/Control-Your-Fireplace-With-HomeKit-and-Alexa/
+
+1. sudo npm install -g homebridge-http-irblaster
+
+# configuration
+
+The configuration is very straight forward, you need just a Button name and the URL to control the blaster.  
+
+Example config.json:
+
+```
+{
+      "accessory": "http-irblaster",
+      "name": "Power",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=538BC81:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Preset 1",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=5380835:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Preset 2",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=53888B5:PANASONIC:48"
+        },
+{
+      "accessory": "http-irblaster",
+      "name": "Tuner",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=5202500:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Muting",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=5004C49:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Vol +",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=5000401:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Vol -",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=5008481:PANASONIC:48"
+        } ,
+{
+      "accessory": "http-irblaster",
+      "name": "Music Port",
+      "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=500595C:PANASONIC:48"
+        },
+{
+      "accessory": "http-irblaster",
+      "name": "Fireplace",
+      "url": "http://192.168.1.175/json?simple=1",
+      "on_busy": 28000,
+      "off_busy": 5000,
+      "up_busy": 600,
+      "down_busy": 600,
+      "start": 100,
+      "steps": 10,
+      "rdelay": 600,
+      "off_data": [{
+        "type": "raw",
+        "out": 1,
+        "khz": 500,
+        "data": [200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200],
+        "pulse": 10,
+        "pdelay": 30
+      }],
+      "on_data": [{
+        "type": "raw",
+        "out": 1,
+        "data": [200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 700, 200, 200, 700, 200, 700, 700, 200, 700, 200],
+        "khz": 500,
+        "pulse": 10,
+        "pdelay": 30
+      }],
+      "up_data": [{
+        "type": "raw",
+        "out": 1,
+        "data": [200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 700, 200],
+        "khz": 500,
+        "pulse": 10,
+        "pdelay": 30
+      }],
+      "down_data": [{
+        "type": "raw",
+        "out": 1,
+        "data": [200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 200, 700, 200, 700, 200, 700, 200, 700, 200, 700, 200, 700, 200, 700, 200, 700],
+        "khz": 500,
+        "pulse": 10,
+        "pdelay": 30
+      }]
+    }
+}
+```
+
+Please note, this URL is an example from my configuration controlling my Panasonic SA-PM53 mini system.  These will not work for you, unless you have the same stereo. And the fireplace is one with a radio remote [https://valorfireplaces.com/features/remote-controls.php] with this FCC ID Rtd-g6rh.  To control the fireplace, I just wired in a 315MHZ Transmitter module in place of the LED/Transistor.
+
+# options
+
+* stateful - The default behavior of the plugin is to turn itself off one second after being turned on. However you may want to create a dummy switch that remains on and must be manually turned off.  ie "stateful": true
+* busy - How long should the switch stay on in HomeKit ( in seconds ),  defaults to 1 second.
+* name" - Name of the device
+* "url" - URL of the device, including any options ie "http://192.168.1.175/json?simple=1",
+* "on_busy" - How long the device should be busy for when turning on, in milliseconds ie 30000
+* "off_busy" - How long the device should be busy for when turning off, in milliseconds ie  1000
+* "up_busy" - How long the device should be busy for when turning up, in milliseconds ie 600
+* "down_busy" - How long the device should be busy for when turning down, in milliseconds ie 600
+* start - When turned on, where does the volume start at
+* steps - How many steps for the volume
+
+# Roadmap
+
+1. Support for stateful Switches
+2. Convert to a platform plugin, to simplify the configuration.
+
+# Credits
+
+* mdhiggins - Creating the ESP8266 based IR Blaster, sharing your plans and source.
+* nfarina - For creating the dummy plugin which I used as base for this plugin.
---
+++ index.js
@@ -0,0 +1,315 @@
+// config.json
+
+//{
+//          "accessory": "http-rf-fan-remote",
+//          "name": "Power",
+//	  "url": "http://ESP_869815/msg?repeat=2&rdelay=100&pdelay=1&address=16388&code=538BC81:PANASONIC:48",
+//    "fan_code": "1011100101100100"  // 16 Bits
+//    "dimmable": true
+//    "summer": "01"
+//        }
+
+
+
+
+"use strict";
+
+var debug = require('debug')('RFRemote');
+var request = require("request");
+var Service, Characteristic;
+
+var fanCommands = {
+  fan25: "110111",
+  fan50: "101111",
+  fan75: "101110",
+  fan100:	"011111",
+  Down:	"110011",
+  fan0:	"111101",
+  lightD:  "111110",
+  reverse: "111011",
+  light:  "111110",
+  sync: "111111"
+}
+
+module.exports = function(homebridge) {
+
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-rf-fan-remote", "RFRemote", RFRemote);
+}
+
+function RFRemote(log, config) {
+  this.log = log;
+  this.name = config.name;
+
+  this.remote_code = config.remote_code;
+  this.url = config.url;
+  this.dimmable = config.dimmable || true;
+  this.summer = config.summer || "10";
+
+  // Below are the legacy settings
+
+  this.stateful = config.stateful || false;
+  this.on_busy = config.on_busy || 5;
+  this.off_busy = config.off_busy || 1;
+  this.down_busy = config.down_busy || 1;
+  this.up_busy = config.up_busy || 1;
+  this.rdelay = config.rdelay || 600;
+  this.on_data = config.on_data;
+  this.off_data = config.off_data;
+  this.up_data = config.up_data;
+  this.down_data = config.down_data;
+  this.start = config.start || undefined;
+  this.steps = config.steps || 4;
+  this.count = config.count || 0;
+
+  this.working = Date.now();
+
+
+  debug("Adding Fan", this.name);
+  this._fan = new Service.Fan(this.name);
+  this._fan.getCharacteristic(Characteristic.On)
+    .on('set', this._fanOn.bind(this));
+
+  // Using RotationSpeed as a placeholder for up/down control
+
+  this._fan
+    .addCharacteristic(new Characteristic.RotationSpeed())
+    .on('set', this._fanSpeed.bind(this));
+
+  if (this.start) {
+    this._fan.getCharacteristic(Characteristic.RotationSpeed).updateValue(this.start);
+  }
+
+  debug("Adding Light", this.name);
+  this._light = new Service.Lightbulb(this.name);
+  this._light.getCharacteristic(Characteristic.On)
+    .on('set', this._lightOn.bind(this));
+
+  this._light
+    .addCharacteristic(new Characteristic.Brightness())
+    .on('set', this._lightBrightness.bind(this));
+
+  debug("Add Summer",this.name);
+
+  this._summer = new Service.Switch(this.name);
+  this._summer.getCharacteristic(Characteristic.On)
+    .on('set', this._summer.bind(this));
+
+  if (this.start == undefined && this.on_data && this.up_data)
+    this.resetDevice();
+
+}
+
+RFRemote.prototype.getServices = function() {
+  return [this._fan, this._light];
+}
+
+RFRemote.prototype._setSpeed = function(value, callback) {
+
+  //debug("Device", this._fan);
+
+  this.log("Setting " + this.name + " to " + value);
+
+  var current = this._fan.getCharacteristic(Characteristic.RotationSpeed)
+    .value;
+
+  if (current == undefined)
+    current = this.start;
+
+  if (value == 100 && current == 0) {
+    callback(null, current);
+    return;
+  }
+
+  var _value = Math.floor(value / (100 / this.steps));
+  var _current = Math.floor(current / (100 / this.steps));
+  var delta = Math.round(_value - _current);
+
+  debug("Values", this.name, value, current, delta);
+
+  if (delta < 0) {
+    // Turn down device
+    this.log("Turning down " + this.name + " by " + Math.abs(delta));
+    this.httpRequest("down", this.url, this.down_data, Math.abs(delta) + this.count, this.down_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('IR Blast failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('IR Blast succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else if (delta > 0) {
+
+    // Turn up device
+    this.log("Turning up " + this.name + " by " + Math.abs(delta));
+    this.httpRequest("up", this.url, this.up_data, Math.abs(delta) + this.count, this.up_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('IR Blast failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('IR Blast succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+
+  } else {
+    this.log("Not controlling " + this.name, value, current, delta);
+    callback();
+  }
+}
+
+
+RFRemote.prototype._fanOn = function(on, callback) {
+
+  this.log("Setting " + this.name + " to " + on);
+
+  if (on && !this.stateful) {
+    setTimeout(function() {
+      this._fan.setCharacteristic(Characteristic.On, 0);
+    }.bind(this), this.on_busy * 1000);
+  }
+
+  if (on) {
+    this.httpRequest("toggle", this.url, this.data, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('IR Blast failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('IR Blast succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else {
+    callback();
+  }
+}
+
+// An actual on/off switch
+
+RFRemote.prototype._setState = function(on, callback) {
+
+  this.log("Turning " + this.name + " to " + on);
+
+  debug("_setState", this.name, on, this._fan.getCharacteristic(Characteristic.On).value);
+
+  if (on && !this._fan.getCharacteristic(Characteristic.On).value) {
+    this.httpRequest("on", this.url, this.on_data, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('IR Blast failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('IR Blast succeeded!', this.url);
+        var current = this._fan.getCharacteristic(Characteristic.RotationSpeed)
+          .value;
+        if (current != this.start && this.start != undefined) {
+          debug("Setting level after turning on ", this.start);
+          this._fan.getCharacteristic(Characteristic.RotationSpeed).updateValue(this.start);
+        }
+        callback();
+      }
+    }.bind(this));
+  } else if (!on && this._fan.getCharacteristic(Characteristic.On).value) {
+    this.httpRequest("off", this.url, this.off_data, 1, this.off_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('IR Blast failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('IR Blast succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else {
+    debug("Do nothing");
+    callback();
+  }
+}
+
+RFRemote.prototype.resetDevice = function() {
+  debug("Reseting volume on device", this.name);
+  this.httpRequest("on", this.url, this.on_data, 1, this.on_busy, function(error, response, responseBody) {
+
+    setTimeout(function() {
+      this.httpRequest("down", this.url, this.down_data, this.steps, this.down_busy, function(error, response, responseBody) {
+
+        setTimeout(function() {
+          this.httpRequest("up", this.url, this.up_data, 2, this.up_busy, function(error, response, responseBody) {
+
+            setTimeout(function() {
+              this.httpRequest("off", this.url, this.off_data, 1, this.off_busy, function(error, response, responseBody) {
+                this._fan.getCharacteristic(Characteristic.RotationSpeed).updateValue(2);
+              }.bind(this));
+            }.bind(this), this.off_busy);
+
+          }.bind(this));
+
+        }.bind(this), this.steps * this.down_busy);
+      }.bind(this));
+
+    }.bind(this), this.on_busy);
+  }.bind(this));
+
+
+}
+
+RFRemote.prototype.httpRequest = function(name, url, data, count, sleep, callback) {
+  //debug("url",url,"Data",data);
+  // Content-Length is a workaround for a bug in both request and ESP8266WebServer - request uses lower case, and ESP8266WebServer only uses upper case
+
+  debug("HttpRequest", name, url, count, sleep);
+
+  //debug("time",Date.now()," ",this.working);
+
+  if (Date.now() > this.working) {
+    this.working = Date.now() + sleep * count;
+
+    if (data) {
+
+      data[0].repeat = count;
+      data[0].rdelay = this.rdelay;
+
+      var body = JSON.stringify(data);
+      debug("Body", body);
+      request({
+          url: url,
+          method: "POST",
+          timeout: 5000,
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': body.length
+          },
+          body: body
+        },
+        function(error, response, body) {
+          if (response) {
+            debug("Response", response.statusCode, response.statusMessage);
+          } else {
+            debug("Error", name, url, count, sleep, callback, error);
+          }
+
+          if (callback) callback(error, response, body);
+        }.bind(this));
+    } else {
+      // Simple URL Format
+      request({
+          url: url,
+          method: "GET",
+          timeout: 500
+        },
+        function(error, response, body) {
+          if (response) {
+            debug("Response", response.statusCode, response.statusMessage);
+          } else {
+            debug("Error", error);
+          }
+
+          if (callback) callback(error, response, body);
+        })
+    }
+  } else {
+    debug("NODEMCU is busy", name);
+    if (callback) callback(new Error("Device Busy"));
+  }
+}
