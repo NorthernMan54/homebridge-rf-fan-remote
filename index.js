b4122cb2d59cf789eab8abf8c8d572c398a04458
NorthernMan54 <buyminivan-northern@yahoo.ca>
2017-12-02 19:29:03 -0500

    End of Day 1

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ var request = require("request");
 var Service, Characteristic;
 
 var fanCommands = {
+  fan0: "111101",
   fan25: "110111",
   fan50: "101111",
   fan75: "101110",
-  fan100:	"011111",
-  Down:	"110011",
-  fan0:	"111101",
-  lightD:  "111110",
+  fan100: "011111",
+  Down: "110011",
+  lightD: "111110",
   reverse: "111011",
-  light:  "111110",
+  lightND: "111110",
   sync: "111111"
 }
 
@@ -47,6 +47,17 @@ function RFRemote(log, config) {
   this.url = config.url;
   this.dimmable = config.dimmable || true;
   this.summer = config.summer || "10";
+  this.out = config.out || 1;
+
+  //
+
+  if (this.dimmable) {
+    fanCommands.light = fanCommands.lightD;
+    fanCommands.dimmable = "1";
+  } else {
+    fanCommands.light = fanCommands.lightND;
+    fanCommands.dimmable = "0";
+  }
 
   // Below are the legacy settings
 
@@ -76,7 +87,10 @@ function RFRemote(log, config) {
 
   this._fan
     .addCharacteristic(new Characteristic.RotationSpeed())
-    .on('set', this._fanSpeed.bind(this));
+    .on('set', this._fanSpeed.bind(this))
+    .setProps({
+      minStep: 25
+    });
 
   if (this.start) {
     this._fan.getCharacteristic(Characteristic.RotationSpeed).updateValue(this.start);
@@ -91,11 +105,11 @@ function RFRemote(log, config) {
     .addCharacteristic(new Characteristic.Brightness())
     .on('set', this._lightBrightness.bind(this));
 
-  debug("Add Summer",this.name);
+  debug("Add Summer", this.name);
 
-  this._summer = new Service.Switch(this.name);
-  this._summer.getCharacteristic(Characteristic.On)
-    .on('set', this._summer.bind(this));
+  this._summer = new Service.Switch(this.name + " Summer");
+//  this._summer.getCharacteristic(Characteristic.On)
+//    .on('set', this._summer.bind(this));
 
   if (this.start == undefined && this.on_data && this.up_data)
     this.resetDevice();
@@ -103,10 +117,130 @@ function RFRemote(log, config) {
 }
 
 RFRemote.prototype.getServices = function() {
-  return [this._fan, this._light];
+  return [this._fan, this._light, this._summer];
 }
 
-RFRemote.prototype._setSpeed = function(value, callback) {
+RFRemote.prototype._fanOn = function(on, callback) {
+
+  this.log("Setting " + this.name + " to " + on);
+
+  if (on) {
+
+    this.httpRequest("toggle", this.url, fanCommands.fan25, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else {
+    this.httpRequest("toggle", this.url, fanCommands.fan0, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  }
+}
+
+RFRemote.prototype._fanSpeed = function(value, callback) {
+
+  this.log("Setting " + this.name + " to " + value);
+
+  var command;
+  switch (true) {
+    case (value < 15):
+      command = fanCommands.fan0;
+      break;
+    case (value < 40):
+      command = fanCommands.fan25;
+      break;
+    case (value < 65):
+      command = fanCommands.fan50;
+      break;
+    case (value < 90):
+      command = fanCommands.fan75;
+      break;
+    case (value < 101):
+      command = fanCommands.fan100;
+      break;
+  }
+
+  this.httpRequest("toggle", this.url, command, 1, this.on_busy, function(error, response, responseBody) {
+    if (error) {
+      this.log('RFRemote failed: %s', error.message);
+      callback(error);
+    } else {
+      debug('RFRemote succeeded!', this.url);
+      callback();
+    }
+  }.bind(this));
+
+}
+
+RFRemote.prototype._lightOn = function(on, callback) {
+
+  this.log("Setting " + this.name + " to " + on);
+
+  if (on) {
+
+    this.httpRequest("toggle", this.url, fanCommands.light, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else {
+    this.httpRequest("toggle", this.url, fanCommands.light, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  }
+}
+
+RFRemote.prototype._summer = function(on, callback) {
+
+  this.log("Setting " + this.name + " to " + on);
+
+  if (on) {
+    this.summer = "10";
+    this.httpRequest("toggle", this.url, fanCommands.reverse, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  } else {
+    this.summer = "00";
+    this.httpRequest("toggle", this.url, fanCommands.reverse, 1, this.on_busy, function(error, response, responseBody) {
+      if (error) {
+        this.log('RFRemote failed: %s', error.message);
+        callback(error);
+      } else {
+        debug('RFRemote succeeded!', this.url);
+        callback();
+      }
+    }.bind(this));
+  }
+}
+
+RFRemote.prototype._lightBrightness = function(value, callback) {
 
   //debug("Device", this._fan);
 
@@ -134,10 +268,10 @@ RFRemote.prototype._setSpeed = function(value, callback) {
     this.log("Turning down " + this.name + " by " + Math.abs(delta));
     this.httpRequest("down", this.url, this.down_data, Math.abs(delta) + this.count, this.down_busy, function(error, response, responseBody) {
       if (error) {
-        this.log('IR Blast failed: %s', error.message);
+        this.log('RFRemote failed: %s', error.message);
         callback(error);
       } else {
-        debug('IR Blast succeeded!', this.url);
+        debug('RFRemote succeeded!', this.url);
         callback();
       }
     }.bind(this));
@@ -147,10 +281,10 @@ RFRemote.prototype._setSpeed = function(value, callback) {
     this.log("Turning up " + this.name + " by " + Math.abs(delta));
     this.httpRequest("up", this.url, this.up_data, Math.abs(delta) + this.count, this.up_busy, function(error, response, responseBody) {
       if (error) {
-        this.log('IR Blast failed: %s', error.message);
+        this.log('RFRemote failed: %s', error.message);
         callback(error);
       } else {
-        debug('IR Blast succeeded!', this.url);
+        debug('RFRemote succeeded!', this.url);
         callback();
       }
     }.bind(this));
@@ -161,34 +295,6 @@ RFRemote.prototype._setSpeed = function(value, callback) {
   }
 }
 
-
-RFRemote.prototype._fanOn = function(on, callback) {
-
-  this.log("Setting " + this.name + " to " + on);
-
-  if (on && !this.stateful) {
-    setTimeout(function() {
-      this._fan.setCharacteristic(Characteristic.On, 0);
-    }.bind(this), this.on_busy * 1000);
-  }
-
-  if (on) {
-    this.httpRequest("toggle", this.url, this.data, 1, this.on_busy, function(error, response, responseBody) {
-      if (error) {
-        this.log('IR Blast failed: %s', error.message);
-        callback(error);
-      } else {
-        debug('IR Blast succeeded!', this.url);
-        callback();
-      }
-    }.bind(this));
-  } else {
-    callback();
-  }
-}
-
-// An actual on/off switch
-
 RFRemote.prototype._setState = function(on, callback) {
 
   this.log("Turning " + this.name + " to " + on);
@@ -198,10 +304,10 @@ RFRemote.prototype._setState = function(on, callback) {
   if (on && !this._fan.getCharacteristic(Characteristic.On).value) {
     this.httpRequest("on", this.url, this.on_data, 1, this.on_busy, function(error, response, responseBody) {
       if (error) {
-        this.log('IR Blast failed: %s', error.message);
+        this.log('RFRemote failed: %s', error.message);
         callback(error);
       } else {
-        debug('IR Blast succeeded!', this.url);
+        debug('RFRemote succeeded!', this.url);
         var current = this._fan.getCharacteristic(Characteristic.RotationSpeed)
           .value;
         if (current != this.start && this.start != undefined) {
@@ -214,10 +320,10 @@ RFRemote.prototype._setState = function(on, callback) {
   } else if (!on && this._fan.getCharacteristic(Characteristic.On).value) {
     this.httpRequest("off", this.url, this.off_data, 1, this.off_busy, function(error, response, responseBody) {
       if (error) {
-        this.log('IR Blast failed: %s', error.message);
+        this.log('RFRemote failed: %s', error.message);
         callback(error);
       } else {
-        debug('IR Blast succeeded!', this.url);
+        debug('RFRemote succeeded!', this.url);
         callback();
       }
     }.bind(this));
@@ -267,11 +373,11 @@ RFRemote.prototype.httpRequest = function(name, url, data, count, sleep, callbac
 
     if (data) {
 
-      data[0].repeat = count;
-      data[0].rdelay = this.rdelay;
+  //    data[0].repeat = count;
+  //    data[0].rdelay = this.rdelay;
 
-      var body = JSON.stringify(data);
-      debug("Body", body);
+      var body = JSON.stringify(buildBody(command));
+      //      debug("Body", body);
       request({
           url: url,
           method: "POST",
@@ -312,4 +418,20 @@ RFRemote.prototype.httpRequest = function(name, url, data, count, sleep, callbac
     debug("NODEMCU is busy", name);
     if (callback) callback(new Error("Device Busy"));
   }
+}
+
+function buildBody(command) {
+  // This is the command structure for
+  var remoteCommand = "0" + this.summer + this.remote_code + fanCommands.dimmable + command;
+  debug("This is the command", remoteCommand);
+  var body = [{
+    "type": "raw",
+    "out": this.out,
+    "khz": 500,
+    "data": [200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200, 200, 700, 200, 700, 200, 700, 700, 200, 700, 200, 200, 700, 700, 200, 700, 200, 700, 200],
+    "pulse": 10,
+    "pdelay": 30
+  }];
+  debug("This is the body", body);
+  return body;
 }
